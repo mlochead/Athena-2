c5116de96c4ac758b3d80e3cb1e5e02c6480df43
Michael Lochead <mlochead@gmail.com>
2019-11-07 13:09:10 -0700

    Catalog

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "scalr_aws_secret_key" {}
+
+variable "scalr_aws_access_key" {}
+
+variable "region" {
+description = "Region"
+}
+
+variable "instance_type" {
+description = "Instance Type"
+}
+
+variable "subnet" {
+description = "Subnet ID"
+}
+
+variable "sg" {
+description = "AWS Secruity Group"
+type = list(string)
+}
+
+variable "key" {
+description = "AWS Key"
+}
+
+variable "vpc_id" {
+description = "VPC"
+}
+
+variable "ami" {
+description = "AMI"
+}
